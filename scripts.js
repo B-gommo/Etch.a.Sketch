0babf3ded908955791ea6377872169ac0147499f
Brad <bgommo.code@gmail.com>
2023-01-23 16:03:30 +0000

    Add the JS for creation of canvas Div'scripts
    Added fillGrid() function to fill up the canvas
    space with the required Div's on load.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,30 @@
+// slider values and grid creation
 const myRange = document.querySelector("#myRange");
 const sliderOutput = document.querySelector(".firstOutput");
 const sliderOutputTwo = document.querySelector(".secondOutput");
 sliderOutput.innerHTML = myRange.value;
 sliderOutputTwo.innerHTML = myRange.value;
 
-myRange.oninput = function() {
+const canvas = document.getElementById("canvas");
+
+function fillGrid() {
+    const gridResult = `${myRange.value}` * `${myRange.value}`
+    for (let gridLines = 1; gridLines<gridResult; gridLines++) {
+        console.log(`${myRange.value}`);
+        const cell = document.createElement("div");
+        canvas.appendChild(cell);
+    }
+}
+
+fillGrid();
+
+
+canvas.style.cssText = `display: grid; grid-template: repeat(${myRange.value},1fr)/repeat(${myRange.value},1fr)`;
+
+myRange.oninput = function () {
     sliderOutput.innerHTML = myRange.value;
     sliderOutputTwo.innerHTML = myRange.value;
+    canvas.style.gridTemplate = `repeat(${myRange.value},1fr)/repeat(${myRange.value},1fr)`;
 }
+
+
