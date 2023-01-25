b61d0dd041846bf914308eb2939c065b630f712c
Brad <bgommo.code@gmail.com>
2023-01-25 13:58:21 +0000

    Add event listeners for drawing on canvas
    Added event listeners to fillGrid() to allow for
    background color change of #canvas Div's when mouse
    event is clicked and dragged. Added backgroundColor()
    in support of this.
    
    Amended removeChildNodes() to clearCanvas() and simplified
    the removal of child nodes.
    
    Changed max size on slider to 60 as 100 was making
    the slider lag due to large number of div's created.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// slider values and grid creation
 const myRange = document.querySelector("#myRange");
 const sliderOutput = document.querySelector(".firstOutput");
 const sliderOutputTwo = document.querySelector(".secondOutput");
@@ -6,33 +5,42 @@ sliderOutput.innerHTML = myRange.value;
 sliderOutputTwo.innerHTML = myRange.value;
 
 const canvas = document.getElementById("canvas");
+canvas.style.cssText = `display: grid; grid-template: repeat(${myRange.value},1fr)/repeat(${myRange.value},1fr)`;
 
 function fillGrid() {
     const gridResult = `${myRange.value}` * `${myRange.value}`
     for (let gridLines = 1; gridLines<=gridResult; gridLines++) {
         const cell = document.createElement("div");
+        cell.addEventListener('mouseover', backgroundColor);
+        cell.addEventListener('mousedown', backgroundColor);
+        cell.setAttribute('draggable', 'false');
         canvas.appendChild(cell);
+    }
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function backgroundColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    else{
+    e.target.style.backgroundColor = 'black';
     }
 }
 
 fillGrid();
 
-
-function removeAllChildNodes() {
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.firstChild);
-    }
+function clearCanvas() {
+    canvas.innerHTML ='';
 }
-
 
 canvas.style.cssText = `display: grid; grid-template: repeat(${myRange.value},1fr)/repeat(${myRange.value},1fr)`;
 
 myRange.oninput = function () {
-    removeAllChildNodes();
+    clearCanvas();
     sliderOutput.innerHTML = myRange.value;
     sliderOutputTwo.innerHTML = myRange.value;
     canvas.style.gridTemplate = `repeat(${myRange.value},1fr)/repeat(${myRange.value},1fr)`;
-    fillGrid()
+    fillGrid();
 }
-
-
